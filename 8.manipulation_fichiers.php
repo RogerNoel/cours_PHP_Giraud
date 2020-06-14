0b0c925180fb7cb4aebc62b0619114dbf8e4eb13
RogerNoel <noel.roger.pro@gmail.com>
2020-06-14 23:07:47 +0200

    test

---
+++ 8.manipulation_fichiers.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manipulation de fichiers</title>
+</head>
+<body>
+    <h1>Manipuler des fichiers en PHP</h1>
+    214
+</body>
+</html>
