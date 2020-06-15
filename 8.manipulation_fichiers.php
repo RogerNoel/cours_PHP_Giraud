8ef03d82264daaf83dcbb4446b5bbe710eaf4ec1
RogerNoel <noel.roger.pro@gmail.com>
2020-06-15 23:34:10 +0200

    travail manipulation fichiers

--- 8.manipulation_fichiers.php
+++ 8.manipulation_fichiers.php
@@ -7,6 +7,118 @@
 </head>
 <body>
     <h1>Manipuler des fichiers en PHP</h1>
-    214
+    <p>Nous allons apprendre à ouvrir et lire un fichier déjà existant ou à créer des fichiers de différents formats (fichiers texte, etc.) grâce aux fonctions PHP et à écrire des informations dedans.</br>
+    Laa manipulation de fichiers est un sujet complexe et pour lequel les problématiques peuvent être très différentes et très précises. Nous allons présenter les cas de manipulation de fichiers les plus généraux et les fonctions communes liées.</p>
+    <p>En PHP, nous allons pouvoir manipuler différents types de fichiers comme des fichiers texte (au format .txt) ou des fichiers image par exemple.</br>
+    Nous allons particulièrement nous intéresser à la manipulation de fichiers texte puisque ce sont les fichiers qu’on manipule le plus souvent en pratique. Jusqu’à présent, nous n’avons appris à stocker des informations que de manière temporaire en utilisant les variables « classiques », les cookies ou les sessions. L’utilisation de fichiers en PHP va nous permettre entre autres de stocker de façon définitive des informations.</br>
+    Les fichiers vont donc nous offrir une alternative aux bases de données mais on préférera dans la majorité des cas utiliser les bases de données plutôt que les fichiers pour enregistrer les données.</br>
+    Il reste néanmoins intéressant d’apprendre à stocker des données dans des fichiers car on préfèrera <u>dans certains cas</u> stocker des données dans des fichiers plutôt que dans des bases de données. Par ailleurs, la manipulation de fichiers ne se limite pas au stockage de données : nous allons pouvoir effectuer toutes sortes d’opérations sur nos fichiers ce qui représente un avantage indéniable.</p>
+    <h2>Lire un fichier PHP</h2>
+    <p>L’une des opérations de base relative aux fichiers en PHP va tout simplement être de <strong>lire le contenu d’un fichier</strong>. Il existe deux façons de faire cela :
+        <ul>
+            <li>on peut soit lire le contenu d’un fichier morceau par morceau, chose que nous apprendrons à faire dans la prochaine leçon,</li>
+            <li>soit lire un fichier entièrement c’est-à-dire afficher tout son contenu d’un coup.</li>
+        </ul>
+    Pour faire cela, on va pouvoir utiliser l’une des fonctions suivantes :
+        <ul>
+            <li>La fonction <strong>file_get_contents()</strong> qui va retourner le contenu du fichier dans une chaine de caractères qu’il faudra echo pour afficher ou la valeur booléenne false en cas d’erreur.</li>
+            <li>La fonction <strong>file()</strong> est identique à la seule différence que le contenu du fichier sera renvoyé dans un tableau numéroté. Chaque ligne de notre fichier sera un nouvel élément de tableau.</li>
+            <li>La fonction <strong>readfile ()</strong> va directement lire et afficher le contenu de notre fichier.</li>
+        </ul>
+    <u>On va devoir passer le chemin relatif menant au fichier qu’on souhaite lire en argument de chacune de ces fonctions.</u>
+    Pour illustrer le fonctionnement de ces trois fonctions, créons un premier fichier de texte txt.</p>
+    <?php
+        echo '1. Utilisation de <em>file_get_contents()</em>: '. file_get_contents('texte.txt').'</br></br>';
+        echo '2. Utilisation de <em>file()</em>: </br>';
+        print_r(file('texte.txt'));
+        echo '</br></br>3. Utilisation de <em>readfile()</em>: </br>';
+        readfile('texte.txt');
+    ?>
+    <p>Nous avons utilisé les trois fonctions; comme on peut le voir, file_get_contents() (cas 1) et readfile() (cas 3) produisent un résultat similaire mais il faut echo le résultat renvoyé par file_get_contents() pour l’afficher tandis que readfile() affiche directement le résultat.</br>
+    La fonction file_get_contents() nous donne donc davantage de contrôle sur la valeur renvoyée puisqu’on va pouvoir l’enfermer dans une variable pour la manipuler à loisir.</p>
+    <h2>Conserver la mise en forme d'un fichier avant affichage</h2>
+    <p>Dans l’exemple ci-dessus, <em>on s’aperçoit que les retours à la ligne et les sauts de ligne contenus dans notre fichier texte ne sont pas conservés</em> lors de l’affichage du texte dans le navigateur. Cela est normal puisque <u>nos fonctions vont renvoyer le texte tel quel sans y ajouter des éléments HTML indiquant de nouvelles lignes ou des sauts de ligne</u> et donc le navigateur va afficher le texte d’une traite.</br>
+    Pour conserver la mise en forme du texte, on va pouvoir utiliser la fonction <strong>nl2br()</strong> qui va se charger d’ajouter des éléments <em>br</em> devant chaque nouvelle ligne de notre texte.</br>
+    Nous allons pouvoir utiliser cette fonction <em>sur le résultat renvoyé par file_get_contents()</em> avant de l’afficher. En revanche, on ne va pas pouvoir l’utiliser avec readfile() puisque cette fonction va directement afficher le résultat sans que nous puissions exercer un quelconque contrôle dessus.</p>
+    <?php
+        echo 'Utilisation de nl2br(): </br>';
+        echo nl2br(file_get_contents('texte.txt'));
+    ?>
+    <h2>Ouvrir, lire et fermer un fichier</h2>
+    <p>Dans le chapitre précédent, nous avons appris à lire un fichier texte entièrement.</br>
+    Dans ce chapitre, il s'agit de ne lire et ne récupérer qu’une partie d’un fichier.</p>
+    <p>Pour lire un fichier partie par partie, il faut d'abord l’ouvrir.</br>
+    Pour ouvrir un fichier en PHP, nous allons utiliser la fonction <strong>fopen()</strong>.</br>
+    <strong>ATTENTION fopen() ne sert qu'à récupérer la ressource pour la mettre dans une variable afin de la manipuler ensuite.</strong>
+    On va devoir passer le chemin relatif menant à notre fichier ainsi qu’un « mode » en arguments à cette fonction qui va alors retourner en cas de succès une <em>ressource de pointeur de fichier</em>, c’est-à-dire pour le dire <u>plus simplement une ressource qui va nous permettre de manipuler notre fichier.</u></p>
+    <p>Le mode choisi en second argument détermine le type d’accès au fichier et donc les opérations qu’on va pouvoir effectuer sur le fichier. On va pouvoir choisir parmi les modes suivants :
+        <ul>
+            <li>r - pour ouverture en lecture seule</li>
+            <li>r+ - ouverture en lecture et écriture</li>
+            <li>a - ouvre un fichier en écriture seule en conservant les données existantes.Si le fichier n'existe pas, PHP tente de le créer.</li>
+            <li>a+ - ouvre un fichier en lecture et écriture en conservant les données existantes. Si le fichier n'existe pas, PHP tente de le créer.</li>
+            <li>w - ouverture en écriture seule. Si le fichier existe, les infos existantes seront supprimées, s'il n'exispe pas, il sera créé.</li>
+            <li>w+ - ouverture en lecture et écriture. Si le fichier existe, les infos existantes seront supprimées, s'il n'exispe pas, il sera créé.</li>
+            <li>x - créé un nouveau fichier accessible seulement en écriture. Retourne false erreur si le fichier existe déjà.</li>
+            <li>x+ - créé un nouveau fichier accessible en lecture et en écriture. Retourne false erreur si le fichier existe déjà.</li>
+            <li>c - ouvre un fichier pour écriture seulement. Si le fichier n'existe pas, il sera créé. S'il existe, les infos seront conservées.</li>
+            <li>c+ - ouvre un fichier pour lecture et écriture. Si le fichier n'existe pas, il sera créé. S'il existe, les infos seront conservées.</li>
+            <li>e - mode particulier et pas toujours disponible. On n'en parnera pas ici</li>
+        </ul>
+        <strong>Notez</strong> par ailleurs qu’<u>on ajoutera généralement la lettre b au paramètre « mode » de fopen()</u>. Cela permet une meilleure compatibilité et évite les erreurs pour les systèmes qui différencient les fichiers textes et binaires comme Windows par exemple.
+    </p>
+    <h2>Lire un fichier partie par partie</h2>
+    <p>Voici 3 des fonctions qu'on peut utiliser:
+        <ul>
+            <li> <strong>fread()</strong> </li>
+            <li> <strong>fgets()</strong> </li>
+            <li> <strong>fgetc()</strong> </li>
+        </ul>
+    </p>
+    <h3>Utilisation de fread()</h3>
+    <p>On commence par utiliser fopen() pour récupérer la ressource renvoyée par la fonction dans une variable.</br>
+    On passe ensuite le contenu de notre variable à fread() et on lui demande de lire les 15 premiers caractères.</br>
+    Si on veut lire tout le fichier, il faut connaître le nombre de caractères qu'il contient, mais on ne le sait pas... la fonction filesize(), qu'on utilisera à la place d'un nombre de caractères, calculera la taille exacte du fichier.</p>
+    <?php
+        $recuperationDuFichier = fopen('./fichiers/unFichierTexte.txt', 'rb');
+        echo 'Récupération des 15 premiers caractères: '. fread($recuperationDuFichier, 15); // second argument = nombre de caractères à afficher
+    ?>
+    <?php
+        $ressource = fopen('./fichiers/unFichierTexte.txt', 'rb');
+        echo '</br>Lecture de tout le fichier avec la fonction filesize()</br>';
+        echo fread($ressource, filesize('./fichiers/unFichierTexte.txt'));
+    ?>
+    <h3>Utilisation de fgets()</h3>
+    <p>La fonction fgets() va nous permettre de <u>lire un fichier ligne par ligne</u>. On va passer le résultat renvoyé par fopen() en argument de fgets() et à <strong>chaque nouvel appel de la fonction</strong>, une nouvelle ligne du fichier va pouvoir être lue.</br>
+    On peut également préciser de manière facultative un nombre en deuxième argument de fgets() qui représentera un nombre d’octets. La fonction lira alors:
+        <ul>
+            <li>soit le nombre d’octets précisé,</li>
+            <li>soit jusqu’à la fin du fichier,</li>
+            <li>soit jusqu’à arriver à une nouvelle ligne</li>
+            <li>(le premier des trois cas qui va se présenter).</li>
+        </ul>
+    Si aucun nombre n’est précisé, fgets() lira jusqu’à la fin de la ligne.</p>
+    <?php
+    echo 'fgets() va lire le texte ligne par ligne, à chaque appel de la fonction: </br>';
+        $ressourceTexte = fopen('texte.txt', 'rb');
+        echo 'Ligne 1: '.fgets($ressourceTexte).'</br>';
+        echo 'Ligne 2: '.fgets($ressourceTexte).'</br>';
+        echo 'Ligne 3: '.fgets($ressourceTexte).'</br>';
+        echo 'Ligne 4: '.fgets($ressourceTexte).'</br>';
+    ?>
+    <h3>Utilisation de fgetc()</h3>
+    <p>Cette fonction permet de lire un fichier caractère par caractère, par exemple pour récupérer un caractère en particulier ou pour arrêter la lecture lorsqu’on arrive à un certain caractère.</br>
+    fgetc() s’utilise exactement comme fgets(), et chaque nouvel appel à la fonction va nous
+permettre de lire un nouveau caractère de notre fichier. Notez que les espaces sont bien
+entendus considérés comme des caractères.</p>
+<?php
+    echo 'Utilisation de fgetc() qui permet à chaque appel, de récupérer les caractères un par un: </br>';
+    $ressourceTexte = fopen('texte.txt', 'rb');
+    echo 'Premier appel: '. fgetc($ressourceTexte).'</br>';
+    echo 'Second appel: '. fgetc($ressourceTexte).'</br>';
+    echo 'Troisième appel: '. fgetc($ressourceTexte).'</br>';
+    echo 'Quatrième appel: '. fgetc($ressourceTexte).'</br>';
+?>
+    
 </body>
 </html>
