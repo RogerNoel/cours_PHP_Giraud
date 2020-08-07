b45ae108754d259018f74a46a9be6e85761f3762
RogerNoel <noel.roger.pro@gmail.com>
2020-08-07 14:50:01 +0200

    màj

---
+++ 12a.Espaces_de_noms.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Espaces de Noms</title>
+</head>
+<body>
+    <h1>Les espaces de noms</h1>
+</body>
+</html>
